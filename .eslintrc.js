805d8f507789d0d477d789f304d7d2242a38498a
Kirill Neruchev <kirill@neruchev.com>
2021-07-14 18:40:53 +0200

    Configure package

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  parser: '@typescript-eslint/parser',
+  plugins: ['prettier', '@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  rules: {
+    '@typescript-eslint/ban-types': ['error', { types: { '{}': false } }],
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-namespace': 'off',
+    '@typescript-eslint/no-unused-vars': ['error'],
+    'jsx-a11y/anchor-is-valid': 'off',
+    'no-extra-boolean-cast': 'off',
+    'no-useless-escape': 'off',
+    'object-shorthand': 'error',
+  },
+};
